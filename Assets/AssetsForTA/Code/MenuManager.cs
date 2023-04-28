e57a1fdbbb93479b6401fa55f59e866fe37ebf79
AlexeiKonev <alexkonev777@gmail.com>
2023-04-28 16:56:51 +0300

    music added and save system

--- Assets/AssetsForTA/Code/MenuManager.cs
+++ Assets/AssetsForTA/Code/MenuManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
- public  GameObject menuPanel;
+    public GameObject menuPanel;
 
- private void Start() {
-       
- }
+    private void Start()
+    {
+
+    }
     public void SwitchMenu(bool state)
     {
         menuPanel.SetActive(state);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
